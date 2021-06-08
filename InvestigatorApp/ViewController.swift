8b6c8c803398409e00a1c7098dd370298f75ed8a
Artem Akopian <artem7498@gmail.com>
2021-06-08 16:29:12 +1000

    Initial Commit

---
+++ InvestigatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InvestigatorApp
+//
+//  Created by Артём on 6/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
